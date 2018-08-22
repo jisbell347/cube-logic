fe604e241cb67bfa08ed9e13e12a30c4def46a44
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-22 13:43:32 -0600

    Added variables and start method

--- Cube.cs
+++ Cube.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour {
 
+    public float sizeModifier = 2.5f;
+    public string newName = "Cubey";
+    public bool isRotated = false;
+
+    // Used for initialization
+    void Start ()
+    {
+        Debug.Log("Hello world!");
+    }
 
 }
